fc9d35ecaa2361a6aaac120405a9bb8eaca49958
Viktor Ermakov <79091602201@yandex.ru>
2023-02-24 22:36:28 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,54 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        // this.middle = Math.round(((this.max - this.min) / 2) + this.min);
+        this.middle = Math.round((this.max + this.min) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
 module.exports = GuessingGame;
+
+const number = 279;
+const game = new GuessingGame();
+game.setRange(0, 3445)
+
+let result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
